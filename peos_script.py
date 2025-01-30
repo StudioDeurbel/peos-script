c3cf12557910e48a8bf7fa99e929a10f8c9604d3
StudioDeurbel <nikenaukjeandries@gmail.com>
2025-01-30 16:46:17 +0100

    peos script
    Approved by peos! Je hebt Thonny of Python nodig!

---
+++ peos_script.py
@@ -0,0 +1,6 @@
+honden_weg = input("Wil je alle honden verwijderen? ")
+if honden_weg == "ja":
+    print("Oke! Alle honden worden nu verwijderd!")
+if honden_weg == "nee":
+    print("MAAR PEOS WIL VAN WEL DUS ALLE HONDEN WORDEN VERWIJDERD!")
+        
