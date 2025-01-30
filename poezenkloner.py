973ea38c3e1c74e2db5a081deb4c8fcee651a086
StudioDeurbel <nikenaukjeandries@gmail.com>
2025-01-30 17:48:04 +0100

    Add files via upload

---
+++ poezenkloner.py
@@ -0,0 +1,5 @@
+peosclone = int(input("Hoeveel klonen wil je maken? "))
+poesofpeos = input("Wil je een poezen kloon(en) maken of een peos kloon(en)? ")
+print("ok!")
+for _ in range(peosclone):
+    print(poesofpeos)
